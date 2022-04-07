19c5b2835a73501e96ffb6a49d5a77a39a030e68
shahnmah <shahn.mah@gmail.com>
2022-04-07 20:07:43 +0600

    event handler added

--- src/App.js
+++ src/App.js
@@ -4,30 +4,45 @@ import { Button } from 'react-bootstrap';
 import { getAuth } from "firebase/auth";
 import app from './firebase.init';
 import './App.css';
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const handleEmailBlur = e =>{
+    setEmail(e.target.value)
+  }
+  const handlePasswordBlur = e =>{
+    setPassword(e.target.value)
+  }
+  const handleButtonClick = () =>{
+    console.log(email)
+    console.log(password)
+  }
+  
   return (
     <div className="container p-5 w-50 mx-auto">
       <h2>Please Register Hare</h2>
       <Form className=''>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Already Registered" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button onClick={handleButtonClick} variant="primary" type="submit">
           Register
         </Button>
       </Form>
